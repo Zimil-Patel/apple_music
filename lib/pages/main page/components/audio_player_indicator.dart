ba5667f614d33d52c87f88d19e8de1da09c74695
Zimil-Patel <patelzimil2310@gmail.com>
2024-06-14 05:53:14 +0530

    14-06-2024

--- lib/pages/main page/components/audio_player_indicator.dart
+++ lib/pages/main page/components/audio_player_indicator.dart
@@ -25,87 +25,91 @@ class AudioPlayerIndicator extends StatelessWidget {
       builder: (context, snapshot) {
         final Playing? playing = snapshot.data;
 
-        return Container(
-          margin: EdgeInsets.only(
-            bottom: MediaQuery.of(context).padding.bottom + 8,
-            left: 5,
-            right: 5,
-          ),
-          height: 60,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: CupertinoTheme.of(context).barBackgroundColor.withOpacity(1),
-            boxShadow: [
-              BoxShadow(
-                color: shadowColor,
-                blurRadius: 10,
-                offset: const Offset(0, 5),
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              const SizedBox(width: 10),
-              AspectRatio(
-                aspectRatio: 1,
-                child: Container(
-                  margin: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: CupertinoColors.systemGrey,
-                    borderRadius: BorderRadius.circular(4),
-                    image: DecorationImage(
-                      image: AssetImage(playing != null
-                          ? 'assets/album images/${playing.audio.audio.metas.album}.png'
-                          : 'assets/images/music.png') as ImageProvider,
-                      fit: BoxFit.cover,
+        return TweenAnimationBuilder(
+          tween: Tween<double>(begin: 20, end: 60),
+          duration: const Duration(milliseconds: 150),
+          builder: (context, value, child) => Container(
+            margin: EdgeInsets.only(
+              bottom: MediaQuery.of(context).padding.bottom + 8,
+              left: 5,
+              right: 5,
+            ),
+            height: value,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: CupertinoTheme.of(context).barBackgroundColor.withOpacity(1),
+              boxShadow: [
+                BoxShadow(
+                  color: shadowColor,
+                  blurRadius: 10,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                const SizedBox(width: 10),
+                AspectRatio(
+                  aspectRatio: 1,
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: CupertinoColors.systemGrey,
+                      borderRadius: BorderRadius.circular(4),
+                      image: DecorationImage(
+                        image: AssetImage(playing != null
+                            ? 'assets/album images/${playing.audio.audio.metas.album}.png'
+                            : 'assets/images/music.png') as ImageProvider,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              Text(
-                playing != null
-                    ? playing.audio.audio.metas.title ?? 'Playing'
-                    : 'Not Playing',
-                style: TextStyle(
-                  color: CupertinoTheme.of(context).textTheme.textStyle.color,
+                const SizedBox(width: 10),
+                Text(
+                  playing != null
+                      ? playing.audio.audio.metas.title ?? 'Playing'
+                      : 'Not Playing',
+                  style: TextStyle(
+                    color: CupertinoTheme.of(context).textTheme.textStyle.color,
+                  ),
                 ),
-              ),
-              const Spacer(),
-              TweenAnimationBuilder(
-                key: songProviderTrue.playPauseAnimationKey,
-                tween: Tween<double>(
-                  begin: 6,
-                  end: MediaQuery.of(context).size.height * 0.025,
+                const Spacer(),
+                TweenAnimationBuilder(
+                  key: songProviderTrue.playPauseAnimationKey,
+                  tween: Tween<double>(
+                    begin: 6,
+                    end: MediaQuery.of(context).size.height * 0.025,
+                  ),
+                  duration: const Duration(milliseconds: 200),
+                  builder: (context, value, child) => GestureDetector(
+                    onTap: () => songProviderFalse.playOrPauseAudio(),
+                    child: Icon(
+                      songProviderTrue.isPlaying
+                          ? CupertinoIcons.pause_solid
+                          : CupertinoIcons.play_arrow_solid,
+                      size: value,
+                      color: CupertinoTheme.of(context).primaryColor,
+                    ),
+                  ),
                 ),
-                duration: const Duration(milliseconds: 200),
-                builder: (context, value, child) => GestureDetector(
-                  onTap: () => songProviderFalse.playOrPauseAudio(),
-                  child: Icon(
-                    songProviderTrue.isPlaying
-                        ? CupertinoIcons.pause_solid
-                        : CupertinoIcons.play_arrow_solid,
+                const SizedBox(width: 20),
+                TweenAnimationBuilder(
+                  key: songProviderTrue.nextAnimationKey,
+                  tween: Tween<double>(
+                    begin: 10,
+                    end: MediaQuery.of(context).size.height * 0.025,
+                  ),
+                  duration: const Duration(milliseconds: 200),
+                  builder: (context, value, child) => Icon(
+                    CupertinoIcons.forward_fill,
                     size: value,
                     color: CupertinoTheme.of(context).primaryColor,
                   ),
                 ),
-              ),
-              const SizedBox(width: 20),
-              TweenAnimationBuilder(
-                key: songProviderTrue.nextAnimationKey,
-                tween: Tween<double>(
-                  begin: 10,
-                  end: MediaQuery.of(context).size.height * 0.025,
-                ),
-                duration: const Duration(milliseconds: 200),
-                builder: (context, value, child) => Icon(
-                  CupertinoIcons.forward_fill,
-                  size: value,
-                  color: CupertinoTheme.of(context).primaryColor,
-                ),
-              ),
-              const SizedBox(width: 20),
-            ],
+                const SizedBox(width: 20),
+              ],
+            ),
           ),
         );
       },
